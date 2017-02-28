ed3770279611d50fcfb69935775500072ad07674
Maciej Laciak <maciej.laciak@gmail.com>
2017-02-28 23:06:52 +0000

    Adds a function to create a creep if needed.

---
+++ func.buildCreepsIfNeeded.js
@@ -0,0 +1,38 @@
+const chooseCreepBody = require("func.chooseCreepBody");
+const generateCreepName = require("func.generateCreepName");
+
+module.exports = (spawn, rolesSlots) => {
+
+    const existingRolesCounter = new Map();
+
+    const existingRoles = Object.keys(Game.creeps)
+        .map((creepName) => Game.creeps[ creepName ].memory.role);
+
+    existingRoles.forEach((role) => {
+        const prevCount = existingRolesCounter.get(role);
+        const nextCount = prevCount ? prevCount + 1 : 1;
+        existingRolesCounter.set(role, nextCount);
+    });
+
+    const desiredRoles = new Map();
+
+    rolesSlots.forEach((slotsCount, role) => {
+        const existingCount = existingRolesCounter.get(role);
+        let desiredCount = slotsCount;
+        if (existingCount) {
+            desiredCount -= existingCount;
+        }
+        if (desiredCount > 0) {
+            desiredRoles.set(role, desiredCount);
+        }
+    });
+
+    rolesSlots.forEach((slotsCount, role) => {
+        const body = chooseCreepBody();
+        if (!spawn.canCreateCreep(body)) {
+            return;
+        }
+        const name = generateCreepName();
+        spawn.createCreep(body, name, { role });
+    });
+};
