51bb501209cac00f0a5daacf1139c1d87ab1730f
Maciej Laciak <maciej.laciak@gmail.com>
2017-02-28 20:29:50 +0000

    Adds a function to choose a creep body by its role.

---
+++ func.chooseCreepBody.js
@@ -0,0 +1,12 @@
+const roles = require("const.roles");
+
+module.exports = (role) => {
+
+    switch (role) {
+        case roles.BUILDER:
+        case roles.HARVESTER:
+        case roles.UPGRADER:
+        default:
+            return [CARRY, MOVE, WORK]
+    }
+};
