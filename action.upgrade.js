65e48d8be31b03ce13bc753cd4b2430aa4f6f96b
mac80 <maciej.laciak@gmail.com>
2017-03-02 08:45:20 +0000

    Tweaks the upgrade action algorithm.

--- action.upgrade.js
+++ action.upgrade.js
@@ -1,19 +1,32 @@
+const actions = require("const.actions");
+
 module.exports = (creep) => {
 
     const remainingEnergyCapacity = creep.carryCapacity - creep.carry.energy;
+    let currentAction = creep.memory.action;
 
-    if (remainingEnergyCapacity) {
+    if (!currentAction) {
+        currentAction = actions.HARVESTING;
+    } else if (currentAction == actions.HARVESTING && !remainingEnergyCapacity) {
+        currentAction = actions.UPGRADING;
+    } else if (currentAction == actions.UPGRADING && !creep.carry.energy) {
+        currentAction = actions.HARVESTING;
+    }
+
+    if (currentAction == actions.HARVESTING) {
         const sources = creep.room.find(FIND_SOURCES);
         const bestSource = sources[0];
 
         if (creep.harvest(bestSource) == ERR_NOT_IN_RANGE) {
             creep.moveTo(bestSource, {visualizePathStyle: {stroke: "#ffaa00"}});
         }
-    } else {
+    } else if (currentAction == actions.UPGRADING) {
         const controller = creep.room.controller;
 
         if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
             creep.moveTo(controller, {visualizePathStyle: {stroke: '#ffffff'}});
         }
     }
+
+    creep.memory.action = currentAction;
 };
---
+++ const.actions.js
@@ -0,0 +1,5 @@
+module.exports = {
+    BUILDING: "BUILDING",
+    HARVESTING: "HARVESTING",
+    UPGRADING: "UPGRADING"
+};
