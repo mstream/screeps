d14ad5eb54a9707e5ddf042e6c251532eb741cb7
mac80 <maciej.laciak@gmail.com>
2017-03-05 20:59:59 +0000

    Improves creeps  memory cleaning function.

--- func.clearCreepsMemory.js
+++ func.clearCreepsMemory.js
@@ -1,9 +1,13 @@
 module.exports = () => {
 
-    Object.keys(Memory.creeps)
-            .forEach((creepName) => {
-                if (!Game.creeps[creepName]) {
-                    delete Memory.creeps[creepName];
-                }
-            });
+    if (!Memory.creeps) {
+        return;
+    }
+
+    _.keys(Memory.creeps, (creepName) => {
+        const creepDead = !Game.creeps[creepName];
+        if (creepDead) {
+            delete Memory.creeps[creepName];
+        }
+    });
 };
